5d90bfc7a97a340b5693a8f646cf9deb3f2916fe
Shashank Phatkure <shashankphatkure@gmail.com>
2024-12-03 18:25:47 +0530

    updated

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -180,11 +180,6 @@ export default function ClientLayout({ children }) {
                 <BellIcon className="w-5 h-5 text-gray-500" />
                 <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
               </button>
-              <div className="h-6 w-px bg-gray-200"></div>
-              <button className="flex items-center gap-2 hover:bg-gray-100 rounded-lg px-3 py-2 transition-colors">
-                <UserCircleIcon className="w-5 h-5 text-gray-500" />
-                <span className="text-sm text-gray-700">Profile</span>
-              </button>
             </div>
           </div>
         </header>
